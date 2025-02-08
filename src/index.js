7d5a1f9e955aa07f59442656e74ddaa67c40b94e
SheyGrasset <SheilaGrasset@gmail.com>
2025-02-08 16:44:10 +0100

    hola mundo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hola Mundo');
 });
 
 // Set the server to listen on port 3000
